6d4d320c1ace6e9cd0ec8b1bb5adb137c6003beb
Emiliano9G <138831959+Emiliano9G@users.noreply.github.com>
2023-11-14 18:06:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Mi primer proyecto en la consola");
+
+var ArPM="Emiliano";
+var RaPA="Pacheco";
+var AaNC=ArPM.concat(RaPA);
+
+console.log(AaNC);
+
+var T=10;
+var R=5;
+var sd=T+R;
+var fg=T-R;
+var jk=T*R;
+var lñ=T/R;
+
+console.log(sd);
+console.log(fg);
+console.log(jk);
+console.log(lñ);
